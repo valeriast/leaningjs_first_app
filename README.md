75c792482b67346eb430a24220d50d57c1f9d345
Valeria <valeria.s@outlook.com.br>
2022-10-29 13:49:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO List APP with javascript (Learning js)
+
+App to add and delete tasks from a todo list.
+The list is saved in the browser local storage.
+
+This app is part of a JS Course.
---
+++ script.js
@@ -0,0 +1,57 @@
+
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
+
+function renderTarefas(){
+    listElement.innerHTML = "";
+
+    tarefas.map((todo) => {
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(todo);
+        
+        let linkELement = document.createElement("a");
+        linkELement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode("Excluir");
+        linkELement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+        linkELement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
+        liElement.appendChild(tarefaText);
+        liElement.appendChild(linkELement);
+        listElement.appendChild(liElement);
+    });
+}
+
+renderTarefas();
+
+function adicionarTarefas(){
+    if (inputElement.value === ''){
+        alert("Digite alguma tarefa");
+        return false;
+    }else{
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+        renderTarefas();
+        salvarDados();
+    }
+
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas)); 
+}
